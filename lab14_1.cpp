e7a5fe6a8f82ccdc7a03e3760e29373558c82160
JMjirapat <jm_jirapat@hotmail.com>
2021-12-27 22:52:26 +0700

    lab14 - All Complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,27 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double src[], int N, double dest[]){
+    dest[2] = 1;
+    dest[4] = src[0];
+    dest[5] = src[0];
+    double sum = 0;
+    for(int i = 0; i < N; i++){
+        if(src[i] > dest[4])
+            dest[4] = src[i];
+        if(src[i] < dest[5])
+            dest[5] = src[i];
+        sum += src[i];
+        dest[0] += src[i];
+        dest[2] *= src[i];
+        dest[3] += 1/src[i];
+    }
+    dest[0] = dest[0]/N;
+    for(int i = 0; i < N; i++){
+        dest[1] += pow((src[i]-dest[0]),2);
+    }
+    dest[1] = sqrt(dest[1]/N);
+    dest[2] = pow((dest[2]),1.0/N);
+    dest[3] = N/dest[3];
+}
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,27 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool img[][M]){
+    cout << "------------------------------------------------------------------------" << endl;
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M; j++){
+            if(img[i][j] == 1)
+                cout << "*";
+            else   
+                cout << " ";
+        }
+        cout << "|\n";
+    }
+    cout << "------------------------------------------------------------------------" << endl;
+}
+
+void updateImage(bool img[][M], int size, int xpos, int ypos){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(sqrt(pow(i-xpos,2)+pow(j-ypos,2)) <= size-1)
+                img[i][j] = 1;
+        }
+    }
+}
